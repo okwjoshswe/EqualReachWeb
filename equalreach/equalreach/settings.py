e3a36042ba33f001f0fa9b1f521e8736b9f48fad
okwjosh <okwjosh123@gmail.com>
2025-09-22 18:54:43 +0100

    whitnoise

--- equalreach/equalreach/settings.py
+++ equalreach/equalreach/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
